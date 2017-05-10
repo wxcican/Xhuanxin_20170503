b587137eee54fb7ccdecf82ffbdadc0a4fe274df
wxcican <wxcican@qq.com>
2017-05-10 12:06:36 +0800

    通过EaseUi实现聊天，会话，联系人Fragment

--- app/src/main/java/com/fuicuiedu/xc/xhuanxin_20170503/ui/MainActivity.java
+++ app/src/main/java/com/fuicuiedu/xc/xhuanxin_20170503/ui/MainActivity.java
@@ -14,10 +14,20 @@ import android.widget.Toast;
 
 import com.fuicuiedu.xc.xhuanxin_20170503.MyHelper;
 import com.fuicuiedu.xc.xhuanxin_20170503.R;
+import com.fuicuiedu.xc.xhuanxin_20170503.ui.user.ChatActivity;
 import com.fuicuiedu.xc.xhuanxin_20170503.ui.user.SettingFragment;
 import com.fuicuiedu.xc.xhuanxin_20170503.ui.user.TestFragment;
 import com.hyphenate.EMCallBack;
 import com.hyphenate.chat.EMClient;
+import com.hyphenate.chat.EMConversation;
+import com.hyphenate.easeui.domain.EaseUser;
+import com.hyphenate.easeui.ui.EaseContactListFragment;
+import com.hyphenate.easeui.ui.EaseConversationListFragment;
+import com.hyphenate.exceptions.HyphenateException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -27,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.main_vp)
     ViewPager viewPager;
+    private List<String> mContacts = new ArrayList<>();
+    private EaseContactListFragment mContactListFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,12 +56,32 @@ public class MainActivity extends AppCompatActivity {
             switch (position) {
                 //会话
                 case 0:
-                    // TODO: 2017/5/10 0010 显示会话页面
-                    return new TestFragment();
+                    //显示会话列表页面
+                    EaseConversationListFragment conversationListFragment = new EaseConversationListFragment();
+                    conversationListFragment.setConversationListItemClickListener(new EaseConversationListFragment.EaseConversationListItemClickListener() {
+                        @Override
+                        public void onListItemClicked(EMConversation conversation) {
+                            //点击某个人，跳转到聊天页面
+                            ChatActivity.open(MainActivity.this,conversation.conversationId());
+                        }
+                    });
+                    return conversationListFragment;
                 // 通讯录
                 case 1:
-                    // TODO: 2017/5/10 0010 显示通讯录页面
-                    return new TestFragment();
+                    //显示通讯录页面
+                    mContactListFragment = new EaseContactListFragment();
+                    //需要设置联系人列表才能启动fragment
+                    asyncGetContactsFromServer();
+                    //设置item点击事件
+                    mContactListFragment.setContactListItemClickListener(new EaseContactListFragment.EaseContactListItemClickListener() {
+                        @Override
+                        public void onListItemClicked(EaseUser user) {
+                            //跳转到聊天页面
+                            ChatActivity.open(MainActivity.this,user.getUsername());
+                        }
+                    });
+
+                    return mContactListFragment;
                 //设置
                 case 2:
                     //显示设置页面
@@ -83,5 +115,33 @@ public class MainActivity extends AppCompatActivity {
                 viewPager.setCurrentItem(2);
                 break;
         }
+    }
+
+    //异步获取联系人
+    private void asyncGetContactsFromServer(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    //从环信服务器获取到所有联系人
+                    mContacts = EMClient.getInstance().contactManager().getAllContactsFromServer();
+                    //刷新联系人
+                    refreshContacts();
+                } catch (HyphenateException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    //刷新联系人
+    private void refreshContacts(){
+        HashMap<String,EaseUser> hashmap = new HashMap<>();
+        for (String hxId:mContacts){
+            EaseUser user = new EaseUser(hxId);
+            hashmap.put(hxId,user);
+        }
+        mContactListFragment.setContactsMap(hashmap);
+        mContactListFragment.refresh();
     }
 }
